d08de4349174d6116c489d55e22faf27ed53a33a
lrving <luckykento@gmail.com>
2024-09-12 22:27:05 +0900

    Player調整

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
+class Player{
+  constructor(){
+      this.name="";
+      this.LP=8000;     
+      this.password="" 
+      
+  }
+  RegistName(name) {
+      this.name=name;
+  }
+  CalculateLP(num,operator){
+      switch(operator){
+          case 0:
+          case "+":
+          this.decrementLP(-num);
+          break;
+          case 1:
+          case "-":
+          this.decrementLP(num);
+          break;
+          case 2:
+          case "/2":
+          this.decrementLP(this.LP/2);
+      }
+  }
+  decrementLP(num){
+      this.LP=Math.round(this.LP-num);
+      
+  }
+  resetLP(){
+      this.LP=8000;
+  }
+  setPassword(password){
+      this.password=password;
+  }
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const WebSocket = require('ws');
-const Player = require('./player');
+
 const app = express();
 const port = 3000;
 
