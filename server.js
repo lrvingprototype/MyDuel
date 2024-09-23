4113b5cec91e63f5fafac4212b69f8d3be078387
lrving <luckykento@gmail.com>
2024-09-23 16:08:22 +0900

    ログ機能の簡易作成

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ wss.on('connection', (ws) => {
       player.diceroll()
       wss.clients.forEach(client => {
         if (client.readyState === WebSocket.OPEN) {            
-          client.send(JSON.stringify({ type: 'player', playerData:player  }));
+          client.send(JSON.stringify({ type: 'dice', playerData:player  }));
         }
       });
     }
